56e435bf1a2ab125547cafcd02abbf4acfe3d45d
Javier Matos Denizac <javier.matosd@gmail.com>
2023-03-20 23:07:58 +0000

    using main

--- build.x64.release/vcpkg.ps1
+++ build.x64.release/vcpkg.ps1
@@ -3,24 +3,44 @@
 
 $ENV:NODE_OPTIONS="--enable-source-maps"
 
-# setup the postscript file
-# Generate 31 bits of randomness, to avoid clashing with concurrent executions.
-$env:Z_VCPKG_POSTSCRIPT = Join-Path ([System.IO.Path]::GetTempPath()) "VCPKG_tmp_$(Get-Random -SetSeed $PID).ps1"
 
-[string]$VCPKG = "$PSScriptRoot/vcpkg"
-# The variable:IsWindows test is a workaround for $IsWindows not existing in Windows PowerShell
-if (-Not (Test-Path variable:IsWindows) -Or $IsWindows) {
-  $VCPKG += ".exe"
+function resolve {
+    param ( [string] $name )
+    $name = Resolve-Path $name -ErrorAction 0 -ErrorVariable _err
+    if (-not($name)) { return $_err[0].TargetObject }
+    $Error.clear()
+    return $name
 }
 
-& $VCPKG @args
+
+if( $ENV:VCPKG_ROOT ) {
+  $SCRIPT:VCPKG_ROOT=(resolve $ENV:VCPKG_ROOT)
+  $ENV:VCPKG_ROOT=$VCPKG_ROOT
+} else {
+  $SCRIPT:VCPKG_ROOT=(resolve "$HOME/.vcpkg")
+  $ENV:VCPKG_ROOT=$VCPKG_ROOT
+}
+
+# setup the postscript file
+# Generate 31 bits of randomness, to avoid clashing with concurrent executions.
+$env:Z_VCPKG_POSTSCRIPT = resolve "${VCPKG_ROOT}/VCPKG_tmp_$(Get-Random -SetSeed $PID).ps1"
+
+[string]$vcpkgPath = "$PSScriptRoot/vcpkg"
+# The variable:IsWindows test is a workaround for $IsWindows not existing in Windows PowerShell
+if (-not (Test-Path variable:IsWindows) -or $IsWindows) {
+  $vcpkgPath += ".exe"
+}
+
+& $vcpkgPath @args
 
 # dot-source the postscript file to modify the environment
-if (Test-Path $env:Z_VCPKG_POSTSCRIPT) {
-  $postscr = Get-Content -Raw $env:Z_VCPKG_POSTSCRIPT
-  if( $postscr ) {
-    iex $postscr
-  }
+if ($env:Z_VCPKG_POSTSCRIPT -and (Test-Path $env:Z_VCPKG_POSTSCRIPT)) {
+  # write-host (get-content -raw $env:Z_VCPKG_POSTSCRIPT)
+  $content = get-content -raw $env:Z_VCPKG_POSTSCRIPT
 
-  Remove-Item -Force -ea 0 $env:Z_VCPKG_POSTSCRIPT,env:Z_VCPKG_POSTSCRIPT
+  if( $content ) {
+    iex $content
+  }
+  Remove-Item -Force $env:Z_VCPKG_POSTSCRIPT
+  remove-item -ea 0 -force env:Z_VCPKG_POSTSCRIPT
 }
